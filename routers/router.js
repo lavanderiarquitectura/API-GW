05502082e14eaeae1f00027f0cc5fbaf0440bd82
Angel <adavilam@unal.edu.co>
2019-06-17 21:52:47 -0700

    Created token middleware

--- routers/router.js
+++ routers/router.js
@@ -4,11 +4,44 @@ var userService = require('./userService')
 var clothingRegistrationService = require('./clothingRegistrationService')
 var facturationService = require('./facturationService')
 var machineService = require('./machineService')
+const axios = require('axios');
+const userServiceIP = 'http://' + config_data.clothingRegistrationServiceIP + ':8085'
 
 router.use((req, res, next) => {
     console.log("Called: ", req.path)
     next()
 })
+
+
+router.get('/authenticate/:userId/:password', function(req, res) {
+	axios.get(userServiceIP+ "/api/users/" + req.params.userId +"/"+ req.params.password).then(response => {
+		console.log(response)
+	res.json(response.data)       
+          })
+          .catch(error => {
+            res.send(error);
+          });
+});
+
+router.use(function(req, res, next) {
+
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+	console.log(token)
+	console.log(userServiceIP+ "/api/token/" + token)
+	axios.get(userServiceIP+ "/api/token/" + token).then(response => {
+		console.log(response)
+		user = response;
+		next();
+          })
+          .catch(error => {
+            return res.status(403).send({ 
+			success: false, 
+			message: 'Token not provided or invalid.'
+		});
+          });
+	
+});
 
 router.use(clothingRegistrationService)
 router.use(facturationService)
