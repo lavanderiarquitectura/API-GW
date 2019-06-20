62a89f6067ca0df671f405c6cd80f1abdf2d9c49
Angel <adavilam@unal.edu.co>
2019-06-20 07:44:24 -0700

    old

--- routers/facturationService.js
+++ routers/facturationService.js
@@ -4,7 +4,7 @@ const apiAdapter = require('./apiAdapter')
 
 config_data = require('../ipconfig.json')
 
-const BASE_URL = 'http://' + config_data.clothingRegistrationServiceIP + ':4000'
+const BASE_URL = 'http://' + config_data.userServiceIP + ':4000'
 const api = apiAdapter(BASE_URL)
 
 router.get('/fac_prendas', (req, res) => {
